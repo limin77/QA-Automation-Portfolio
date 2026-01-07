8a6b2f900421352da54457efc1ec8ad2acc62ea1
limin77 <mohamad.alimin77@outlook.my>
2026-01-07 23:33:31 +0800

    Refactor: Add Type Hints and Docstrings to LoginPage

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,25 +1,46 @@
-# pages/login_page.py
 from selenium.webdriver.common.by import By
-from pages.base_page import BasePage
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-class LoginPage(BasePage):
-    # ---  1. LOCATORS (The 'Ingredients') ---
-    USERNAME_FIELD = (By.ID, "user-name")
-    PASSWORD_FIELD = (By.ID, "password")
-    LOGIN_BUTTON = (By.ID, "login-button")
-    ERROR_MESSAGE = (By.CSS_SELECTOR, "h3[data-test='error']")
+class LoginPage:
+    # 1. LOCATORS (Private constants)
+    _USERNAME_FIELD = (By.ID, "user-name")
+    _PASSWORD_FIELD = (By.ID, "password")
+    _LOGIN_BUTTON   = (By.ID, "login-button")
 
-    # --- 2. ACTIONS (The 'The Recipes') ---
-    def load(self):
-        """Opens the SauceDemo login page"""
+    def __init__(self, driver: WebDriver):
+        """
+        Initializes the Page Object.
+        Args:
+            driver (WebDriver): The Selenium WebDriver instance.
+        """
+        self.driver = driver
+        # We set a default Explicit Wait of 10 seconds for this page
+        self.wait = WebDriverWait(driver, 10)
+
+    def load(self) -> None:
+        """Navigates the browser to the SauceDemo login page."""
         self.driver.get("https://www.saucedemo.com/")
 
-    def login(self, username, password):
-        """Performs the full login action"""
-        self.find(self.USERNAME_FIELD).send_keys(username)
-        self.find(self.PASSWORD_FIELD).send_keys(password)
-        self.find(self.LOGIN_BUTTON).click()
+    def login(self, username: str, password: str) -> None:
+        """
+        Performs the login action.
+        Args:
+            username (str): The user's email/username.
+            password (str): The user's password.
+        """
+        # Wait for the username field to be visible before typing (Smart Wait)
+        self.wait.until(EC.visibility_of_element_located(self._USERNAME_FIELD)).send_keys(username)
+        
+        # Find and interact with the rest
+        self.driver.find_element(*self._PASSWORD_FIELD).send_keys(password)
+        self.driver.find_element(*self._LOGIN_BUTTON).click()
 
-    def get_error_message(self):
-        """Returns the error text if login fails"""
-        return self.find(self.ERROR_MESSAGE).text
+    def get_url(self) -> str:
+        """
+        Returns the current page URL for validation.
+        Returns:
+            str: The URL currently in the browser address bar.
+        """
+        return self.driver.current_url
